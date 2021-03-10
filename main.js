bda9031c461fefd5a1e2e2904869595d65698c9d
OlhaRevenko <Olya1030345@gmail.com>
2021-03-11 01:36:23 +0200

    first step lol

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 'use strict'
 
 console.log('Hello Node + JS')
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words);
+
+if(str.indexOf('Fox')== -1){
+    console.log('Fox is not in the str');  //Fox = -1, because isn't in the str
+}
+else{
+    console.log('Found');
+}
+
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+  }
+console.log(factorial(5));
